49b1884859cb486805c52c2cc65cf3eff7a63058
asd2d <872122@mail.ru>
2024-07-01 14:58:51 +0300

    добавлен обработчик

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
